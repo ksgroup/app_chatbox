77a7069ef2699de5b46bb386cc08545aad3ed436
ksgroup <ksgroup73@gmail.com>
2014-11-14 15:15:50 +0200

    Test commit

--- lib/dym.php
+++ lib/dym.php
@@ -57,8 +57,8 @@ DebMes('SAY FUNC: '.$ph);
               $passed=SQLSelectOne("SELECT (UNIX_TIMESTAMP(NOW())-UNIX_TIMESTAMP(ADDED)) as PASSED FROM shouts WHERE ID!='".$rec['ID']."' ORDER BY ID DESC LIMIT 1");
               if ($passed['PASSED']>20) { // play intro-sound only if more than 30 seconds passed from the last one
 		    if ($ding == 1){playSoundDym('dingdong', 1, $level);}
-		    if ($ding == 2){playSoundDym('dingdong', 1, $level);}
-		    if ($ding == 3){playSoundDym('dingdong', 1, $level);}
+		    if ($ding == 2){playSoundDym('tone1', 1, $level);}
+		    if ($ding == 3){playSoundDym('tone2', 1, $level);}
 		    if ($ding == 4){playSoundDym('dingdong', 1, $level);}
                   }
            }
@@ -70,39 +70,54 @@ DebMes('SAY FUNC: '.$ph);
                 safe_exec('cscript '.DOC_ROOT.'/rc/sapi.js '.$ph, 1, $level);
            }
         }
+        global $ignorePushover;
+        if (defined('SETTINGS_PUSHOVER_USER_KEY') && SETTINGS_PUSHOVER_USER_KEY && !$ignorePushover) {
+                include_once(ROOT.'lib/pushover/pushover.inc.php');
+                if (defined('SETTINGS_PUSHOVER_LEVEL')){
+                        if($level>=SETTINGS_PUSHOVER_LEVEL) {
+                                postToPushover($ph);
+                        }
+                } elseif ($level>0) {
+                        postToPushover($ph);
+                }
+        }
 
-        global $noPatternMode;
-        if (!$noPatternMode) {
-                include_once(DIR_MODULES.'patterns/patterns.class.php');
-                $pt=new patterns();
-                $pt->checkAllPatterns();
+        global $ignorePushbullet;
+        if (defined('SETTINGS_PUSHBULLET_KEY') && SETTINGS_PUSHBULLET_KEY && !$ignorePushbullet) {
+                include_once(ROOT.'lib/pushbullet/pushbullet.inc.php');
+                if (defined('SETTINGS_PUSHBULLET_PREFIX') && SETTINGS_PUSHBULLET_PREFIX) {
+                 $prefix=SETTINGS_PUSHBULLET_PREFIX.' ';
+                } else {
+                 $prefix='';
+                }
+
+                if (defined('SETTINGS_PUSHBULLET_LEVEL')){
+                        if($level>=SETTINGS_PUSHBULLET_LEVEL) {
+                                postToPushbullet($prefix.$ph);
+                        }
+                } elseif ($level>0) {
+                        postToPushbullet($prefix.$ph);
+                }
+        }
+
+        global $ignoreGrowl;
+        if (defined('SETTINGS_GROWL_ENABLE') && SETTINGS_GROWL_ENABLE && $level>=SETTINGS_GROWL_LEVEL && !$ignoreGrowl) {
+         include_once(ROOT.'lib/growl/growl.gntp.php');
+         $growl = new Growl(SETTINGS_GROWL_HOST, SETTINGS_GROWL_PASSWORD);
+         $growl->setApplication('MajorDoMo','Notifications');
+         //$growl->registerApplication('http://localhost/img/logo.png');
+         $growl->notify($ph);
+        }
+
+        global $ignoreTwitter;
+        if (defined('SETTINGS_TWITTER_CKEY') && SETTINGS_TWITTER_CKEY && !$ignoreTwitter) {
+         postToTwitter($ph);
         }
 
         if (defined('SETTINGS_HOOK_AFTER_SAY') && SETTINGS_HOOK_AFTER_SAY!='') {
          eval(SETTINGS_HOOK_AFTER_SAY);
         }
 
-
-//        if (defined('SETTINGS_PUSHOVER_USER_KEY') && SETTINGS_PUSHOVER_USER_KEY) {
-//                include_once(ROOT.'lib/pushover/pushover.inc.php');
-//                if (defined('SETTINGS_PUSHOVER_LEVEL')){
-//                        if($level>=SETTINGS_PUSHOVER_LEVEL) {
-//                                postToPushover($ph);
-//                        }
-//                } elseif ($level>0) {
-//                        postToPushover($ph);
-//                }
-//        }
-
-//        if (defined('SETTINGS_GROWL_ENABLE') && SETTINGS_GROWL_ENABLE && $level>=SETTINGS_GROWL_LEVEL) {
-//         include_once(ROOT.'lib/growl/growl.gntp.php');
-//         $growl = new Growl(SETTINGS_GROWL_HOST, SETTINGS_GROWL_PASSWORD);
-//         $growl->setApplication('MajorDoMo','Notifications');
-         //$growl->registerApplication('http://localhost/img/logo.png');
-//         $growl->notify($ph);
-//        }
-
-//        postToTwitter($ph);
 
  }
 /**
